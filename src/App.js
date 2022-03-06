771610236908a27fa45e2482dc3ab02d3a59d43c
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-03-06 16:27:48 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="main__wrapper">
+<input className="text__input" type="text" />
+<button type="submit" className="submit__button">добавить задачу</button>
+<div className="item"></div>
+    </div>
+  );
+}
+
+export default App;
