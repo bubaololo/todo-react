bf846652122fd6513528ad30f22f5189b62c8f3d
bubaololo <bubaololo@gmail.com>
2023-08-09 02:48:50 +0600

    ui tabs implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import { getTodos } from "../api";
 
 import Form from "../components/Form";
 import TodoList from "../components/TodoList";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 function App() {
+  const [activeTab, setActiveTab] = useState(0); // Default active tab index
   const [data, setData] = useState([""]);
 
   const { data: todosData, isLoading: isLoadingTodosData } = useQuery(
@@ -14,16 +17,27 @@ function App() {
     () => getTodos()
   );
 
-
-
   useEffect(() => {
     if (!isLoadingTodosData && todosData?.data) {
       setData(todosData.data);
     }
   }, [isLoadingTodosData, todosData?.data]);
+
+  const handleChangeTab = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
+  const tabLabels = ["Personal", "Work", "Shopping"];
+  const filteredData = data.filter(item => item.type === tabLabels[activeTab]);
+
   return (
     <div className="main__wrapper">
       <Form setData={setData} />
+      <Tabs value={activeTab} onChange={handleChangeTab}>
+        {tabLabels.map((label, index) => (
+          <Tab label={label} key={index} />
+        ))}
+      </Tabs>
       <TodoList data={data} />
     </div>
   );
